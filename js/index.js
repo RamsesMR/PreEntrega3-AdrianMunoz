506d573530cc246a63b81847a2b2d23aef20b43c
ramsesmr <adrianramsesmunozrodriguez@gmail.com>
2024-01-05 23:32:30 +0100

    primera copia de seguridad creando div de facturas

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function cargarFactura(){
+
+    let razon_social = document.getElementById("razon-social-ingresos");
+    let dni = document.getElementById("dni-ingresos");
+    let fecha = document.getElementById("fecha-ingresos");
+    let archivo = document.getElementById("archivo-ingresos");
+    let total = document.getElementById("total-ingresos");
+    let irpf = document.getElementById("irpf-ingresos");
+    let iva = document.getElementById("iva-ingresos");
+
+    
+
+
+
+}
